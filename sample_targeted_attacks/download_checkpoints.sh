f756b536339fe5bf65cf6c45a8b417ee18c2de5e
XJ <xj.jiao@gmail.com>
2018-06-19 16:08:39 -0700

    Updated with google_driver_downloader
    Minor fix
    
    Fix on download_checkpoints.sh

--- sample_targeted_attacks/download_checkpoints.sh
+++ sample_targeted_attacks/download_checkpoints.sh
@@ -11,25 +11,10 @@ wget http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz
 tar -xvzf inception_v3_2016_08_28.tar.gz
 rm inception_v3_2016_08_28.tar.gz
 
-# Another copy of inception v3 checkpoint for iter_target_class attack
-mv inception_v3_2016_08_28.tar.gz "${SCRIPT_DIR}/momentum/"
+# Download checkpoints for momentum attack
+# Original file is from Tshinghua http://ml.cs.tsinghua.edu.cn/~yinpeng/nips17/targeted/models.zip
+# I convert it to tar.gz
 cd "${SCRIPT_DIR}/momentum/"
-tar -xvzf inception_v3_2016_08_28.tar.gz
-rm inception_v3_2016_08_28.tar.gz
-# adv_inception_v3
-wget http://download.tensorflow.org/models/adv_inception_v3_2017_08_18.tar.gz
-tar -xvzf adv_inception_v3_2017_08_18.tar.gz
-rm adv_inception_v3_2017_08_18.tar.gz
-# ens-adv_inception_resnet_v2
-wget http://download.tensorflow.org/models/ens_adv_inception_resnet_v2_2017_08_18.tar.gz
-tar -xvzf ens_adv_inception_resnet_v2_2017_08_18.tar.gz
-rm ens_adv_inception_resnet_v2_2017_08_18.tar.gz
-# ens3_adv_inception_v3
-wget http://download.tensorflow.org/models/ens3_adv_inception_v3_2017_08_18.tar.gz
-tar -xvzf ens3_adv_inception_v3_2017_08_18.tar.gz
-rm ens3_adv_inception_v3_2017_08_18.tar.gz
-# ens4_adv_inception_v3
-wget http://download.tensorflow.org/models/ens4_adv_inception_v3_2017_08_18.tar.gz
-tar -xvzf ens4_adv_inception_v3_2017_08_18.tar.gz
-rm ens4_adv_inception_v3_2017_08_18.tar.gz
-
+python ../../google_drive_downloader.py 1TnT-nHf_a375ilDJKWp5jAu3Bjjwpe8y checkpoints.tar.gz
+tar -xvzf checkpoints.tar.gz
+rm checkpoints.tar.gz
