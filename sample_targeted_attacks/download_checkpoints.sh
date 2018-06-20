df7a16c841bfbe9c0e5037079059902eb7546c7c
JXJ <robbie.jiao@gmail.com>
2018-06-19 23:48:38 -0700

    Added toshi_k target_class attack
    Clean up

--- sample_targeted_attacks/download_checkpoints.sh
+++ sample_targeted_attacks/download_checkpoints.sh
@@ -18,3 +18,15 @@ cd "${SCRIPT_DIR}/momentum/"
 python ../../google_drive_downloader.py 1TnT-nHf_a375ilDJKWp5jAu3Bjjwpe8y checkpoints.tar.gz
 tar -xvzf checkpoints.tar.gz
 rm checkpoints.tar.gz
+
+# Download checkpoints for toshi_k target_class attack.
+cd "${SCRIPT_DIR}/target_class_toshi_k/"
+wget http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz
+tar -xvzf inception_v3_2016_08_28.tar.gz
+rm inception_v3_2016_08_28.tar.gz
+wget http://download.tensorflow.org/models/adv_inception_v3_2017_08_18.tar.gz
+tar -xvzf adv_inception_v3_2017_08_18.tar.gz
+rm adv_inception_v3_2017_08_18.tar.gz
+wget http://download.tensorflow.org/models/ens_adv_inception_resnet_v2_2017_08_18.tar.gz
+tar -xvzf ens_adv_inception_resnet_v2_2017_08_18.tar.gz
+rm ens_adv_inception_resnet_v2_2017_08_18.tar.gz
