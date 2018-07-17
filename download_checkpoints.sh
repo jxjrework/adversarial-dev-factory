fd5bf666ece2344240a5a2caf74d0d46a1cab17e
selfstudyjiao <robbie.jiao@gmail.com>
2018-07-16 22:50:32 -0700

    renamed checkpoints downloader

---
+++ download_checkpoints.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# Download checkpoints for sample attacks and defenses.
+sample_attacks/download_checkpoints.sh
+sample_targeted_attacks/download_checkpoints.sh
+sample_defenses/download_checkpoints.sh
