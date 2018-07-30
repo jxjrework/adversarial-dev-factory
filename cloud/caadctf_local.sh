d7624efb3f9070b54b81bc7b46335b1610bdf865
Chuan Yang <splendidbird@gmail.com>
2018-07-30 16:11:26 -0700

    added caadctf local (non-docker) solution

--- cloud/caadctf_local.sh
+++ cloud/caadctf_local.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y python-pip
@@ -8,7 +8,6 @@ sudo pip install --upgrade pip
 sudo apt-get install -y python-numpy python-scipy python-nose
 #sudo apt-get install -y python-matplotlib ipython ipython-notebook python-pandas python-sympy
 sudo pip install pillow
-sudo pip install -U tensorflow
 
 # gpu installation
 echo "Checking for CUDA and installing."
@@ -24,42 +23,29 @@ fi
 # Enable persistence mode
 nvidia-smi -pm 1
 
-# install docker CE
-sudo apt-get update
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install docker-ce
+# cuDNN installation
+wget https://storage.googleapis.com/caadctf/libcudnn7_7.1.4.18-1%2Bcuda9.0_amd64.deb
+wget https://storage.googleapis.com/caadctf/libcudnn7-dev_7.1.4.18-1%2Bcuda9.0_amd64.deb
+sudo apt install ./libcudnn7_7.1.4.18-1+cuda9.0_amd64.deb
+sudo apt install ./libcudnn7-dev_7.1.4.18-1+cuda9.0_amd64.deb
 
-# install nvidia docker
-# Add the package repositories
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
-  sudo apt-key add -
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
-  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt-get update
-# Install nvidia-docker2 and reload the Docker daemon configuration
-sudo apt-get install -y nvidia-docker2
-sudo pkill -SIGHUP dockerd
-# Test nvidia-smi with the latest official CUDA image
-sudo docker run --runtime=nvidia --rm nvidia/cuda nvidia-smi
+sudo cp -P cuda/include/cudnn.h /usr/local/cuda/include
+sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda/lib64
+sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+
+# below two lines may need manual input
+export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+sudo pip install opencv-python
+sudo pip install -U tensorflow-gpu
+sudo pip install torch torchvision
+sudo pip install cleverhans
 
 # package from github
 echo "start downloading from github"
 sudo git clone https://4530e8b0d1e63be298f40a7e142b5119542c4616@github.com/selfstudyjiao/adversarial-dev-factory.git
-# cd adversarial-dev-factory
-# sudo ./download_data_100.sh
-# cd ..
+sudo adversarial-dev-factory/download_data_100.sh
 
 # revise the package for testing
 # attack
@@ -91,4 +77,4 @@ sudo rm -r -f adversarial-dev-factory/sample_defenses/Random_padding_IresV2
 sudo rm -r -f adversarial-dev-factory/sample_defenses/tv
 
 # start running attack & defense with gpu
-sudo adversarial-dev-factory/run_attacks_and_defenses_gpu.sh
+sudo adversarial-dev-factory/run_attacks_and_defenses_local.sh
